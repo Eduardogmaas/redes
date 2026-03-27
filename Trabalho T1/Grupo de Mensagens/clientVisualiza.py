4c1623a10c6e1fed97598fe99b06bb89ce83e689
Eduardogmaas <eduardogmaas@gmail.com>
2026-03-26 21:48:13 -0300

    aula

---
+++ Trabalho T1/Grupo de Mensagens/clientVisualiza.py
@@ -0,0 +1,4 @@
+# criar dois arquivos para cliente (cada cliente abre 2 terminais)
+# uma digita a mensagem e a thread vai enviar para o servidor
+# o servidor vai receber a mensagem do cliente e incluir numa fila de apresentação 
+# o servidor envia a mensagem para a tela de visualização do grupo de mensagens
---
+++ Trabalho T1/Grupo de Mensagens/servidor.py
@@ -0,0 +1,2 @@
+# o servidor vai receber a mensagem do cliente e incluir numa fila de apresentação 
+# o servidor envia a mensagem para a tela de visualização do grupo de mensagens
